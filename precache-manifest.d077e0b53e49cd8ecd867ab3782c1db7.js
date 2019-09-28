a6e5e6cd784b36d1f45e94c98f869250a2eb71ba
Piotr Pacewicz <piotrp92@gmail.com>
2019-09-28 23:53:06 +0100

    Updates

---
+++ precache-manifest.d077e0b53e49cd8ecd867ab3782c1db7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f602da7027ad39612dafcd9f228b9300",
+    "url": "/Web-Page-in-React/index.html"
+  },
+  {
+    "revision": "e544ca094008006252c7",
+    "url": "/Web-Page-in-React/static/css/main.055dc50c.chunk.css"
+  },
+  {
+    "revision": "9b067239c3345f772860",
+    "url": "/Web-Page-in-React/static/js/2.6c4c384a.chunk.js"
+  },
+  {
+    "revision": "e544ca094008006252c7",
+    "url": "/Web-Page-in-React/static/js/main.55158cf7.chunk.js"
+  },
+  {
+    "revision": "e9dd027c53ffdf4179a2",
+    "url": "/Web-Page-in-React/static/js/runtime-main.272f4baa.js"
+  },
+  {
+    "revision": "86698886498d845bb6223c76a3f984f8",
+    "url": "/Web-Page-in-React/static/media/img1.86698886.jpg"
+  },
+  {
+    "revision": "50ccbad562af07bc90b9ce860a48d176",
+    "url": "/Web-Page-in-React/static/media/img2.50ccbad5.jpg"
+  },
+  {
+    "revision": "c71afc9432d559c0b0109e11795dcbb0",
+    "url": "/Web-Page-in-React/static/media/img3.c71afc94.jpg"
+  },
+  {
+    "revision": "34b67ce4b85d377bb8c299cfb189c582",
+    "url": "/Web-Page-in-React/static/media/img4.34b67ce4.jpg"
+  }
+]);
